5e49dbc356da755f8752aef5ed57d070c72e3fda
EsratHossan <esrathossan7@gmail.com>
2023-02-09 21:23:59 +0600

    added sum and multinumbers factorials

--- findOddSum.js
+++ findOddSum.js
@@ -4,9 +4,27 @@ function getSum(numbers){
         const index = i;
         const element = numbers[index];
         sum = sum + element;
-        console.log(index, element, sum);
+        // console.log(index, element, sum);
     }
+    return sum;
+}
+
+function getOddNumbersOfAnArray(numbers){
+    const oddNumbers = [];
+      for(let i = 0; i < numbers.length; i++){
+        const index = i;
+        const element = numbers[index];
+        if(element % 2 !== 0){
+            console.log(index, element);
+            oddNumbers.push(element);
+        }
+        
+      }
+      return oddNumbers;
 }
 
 const myNumbers = [12, 65, 45, 78, 32, 45, 91];
-getSum(myNumbers);
+const oddNumbers = getOddNumbersOfAnArray(myNumbers);
+console.log(oddNumbers);
+const oddNumberSum = getSum(oddNumbers);
+console.log('odd number sum', oddNumberSum);
